8984e5a24495e86ffcb61fadba6325494c0709e1
Susanne Krkoska <zuzka.krkoska@seznam.cz>
2023-04-29 11:05:24 +0200

    components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './style.css';
+import { Header } from './components/Header/index.js';
+import { Footer } from './components/Footer/index.js';
+import { Banner } from './components/Banner/index.js';
+import { Menu } from './components/Menu/index.js';
+import { Gallery } from './components/Gallery/index.js';
+import { Contact } from './components/Contact/index.js';
 
 console.log('funguju!');
+console.log(Header);
+
+const pageElement = document.createElement('div');
+pageElement.classList.add('page');
+
+const main = document.createElement('main');
+main.append(Banner(), Menu(), Gallery(), Contact());
+
+pageElement.append(Header(), main, Footer());
+
+document.querySelector('#app').append(pageElement);
